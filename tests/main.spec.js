63c39fcb580c59684b4497defbb87f765e16fce5
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2019-02-25 23:01:54 -0300

    Add smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,36 @@
 import { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+
+  describe('smoke tests', () => {
+
+    // search (genério) = + de 1 tipo
+    // searchAlbums
+    // searchArtists
+    // searchTracks
+    // searchPlaylists
+
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+
+  });
 
 });
